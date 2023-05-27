227830a500cf99b3713539ef7285f5a945412717
itsRobertoBorges <robbyborges98@outlook.com>
2023-05-27 15:57:57 -0400

    changed frontend and backend package files to allow both frontend and backend directories to run concurrently

---
+++ bank-app/frontend/src/components/Navbar.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const Navbar = () => {
+    return (
+        <nav className='nav-bar'>
+            <h1>Nav</h1>
+            <ul>
+        
+            </ul>
+        </nav>
+    )
+}
+
+export default Navbar
