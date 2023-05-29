f368baecd5f2abc5ea24c0050e1cfc254b42b5db
itsRobertoBorges <robbyborges98@outlook.com>
2023-05-28 23:05:41 -0400

    redux successfully set up!

--- bank-app/frontend/src/index.js
+++ bank-app/frontend/src/index.js
@@ -4,6 +4,14 @@ import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App";
 
+import { combineReducers } from "redux";
+import authReducer from "./reducers/authReducers";
+import errorReducer from "./reducers/errorReducers";
+export default combineReducers({
+  auth: authReducer,
+  errors: errorReducer
+});
+
 ReactDOM.render(
   <BrowserRouter>
     <App />
--- bank-app/frontend/src/store.js
+++ bank-app/frontend/src/store.js
@@ -1,13 +1,25 @@
 import { createStore, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
+import { combineReducers } from "redux"; // Import combineReducers
+import errorReducers from "./reducers/errorReducers";
+import authReducers from "./reducers/authReducers";
+
 const initialState = {};
 const middleware = [thunk];
+
+// Combine the reducers
+const rootReducer = combineReducers({
+  errorReducers,
+  authReducers,
+  // Add more reducers if needed
+});
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
 const store = createStore(
-  () => [],
+  rootReducer,
   initialState,
-  compose(
-    applyMiddleware(...middleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  )
+  composeEnhancers(applyMiddleware(...middleware))
 );
+
 export default store;
